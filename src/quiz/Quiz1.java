6b82229eb267aebb4602ab0bf55fa450bf57fc30
geunwoo <jkw6275@naver.com>
2024-06-24 16:54:44 +0900

    프로젝트 생성

---
+++ src/quiz/Quiz1.java
@@ -0,0 +1,25 @@
+package quiz;
+
+public class Quiz1 {
+
+	//코드를 보고, num과 result의 값을 예상해서 주서으로 작성하세요.
+	public static void main(String[] args) {
+	
+		int num = -10; //
+		int result = +num; //
+		
+		int num2 = -20; //
+		int result2 = -num2;
+		
+		System.out.println(num);
+		System.out.println(result);
+		System.out.println(num2);
+		System.out.println(result2);
+		
+		
+		
+		
+
+	}
+
+}
